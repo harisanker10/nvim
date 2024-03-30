2ad628cc103f1d85cf6f999ea1df3728a73e37a7
harisanker10 <harisanker10@gmail.com>
2024-03-30 17:24:56 +0530

    Organized file struture

---
+++ lua/hari/keymaps.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = ' '
+vim.g.have_nerd_font = true
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap('n', '<leader>ee', ':Ex<CR>', opts)
+vim.api.nvim_set_keymap('v', '<C-C>', '"+y', opts)
+vim.api.nvim_set_keymap('n', '<C-V>', '"+p', opts)
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+vim.keymap.set('n', '<C-p>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<C-n>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', opts)
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Keybinds to make split navigation easier.
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
