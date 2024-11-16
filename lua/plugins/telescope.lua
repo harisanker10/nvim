c8c60e47329777b237114694cfe4897527b9a109
hari <harisanker10@gmail.com>
2024-11-17 01:29:12 +0530

    stable

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,28 +19,6 @@ return {
   config = function()
     local actions = require 'telescope.actions'
 
-    local open_in_nvim_tree = function(prompt_bufnr)
-      local action_state = require 'telescope.actions.state'
-      local Path = require 'plenary.path'
-      local actions = require 'telescope.actions'
-
-      local entry = action_state.get_selected_entry()[1]
-      local entry_path = Path:new(entry):parent():absolute()
-      actions._close(prompt_bufnr, true)
-      entry_path = Path:new(entry):parent():absolute()
-      entry_path = entry_path:gsub('\\', '\\\\')
-
-      vim.cmd 'NvimTreeClose'
-      vim.cmd('NvimTreeOpen ' .. entry_path)
-
-      file_name = nil
-      for s in string.gmatch(entry, '[^/]+') do
-        file_name = s
-      end
-
-      vim.cmd('/' .. file_name)
-    end
-
     require('telescope').setup {
       defaults = {
         mappings = {
@@ -48,7 +26,6 @@ return {
             ['<C-k>'] = actions.move_selection_previous,
             ['<C-j>'] = actions.move_selection_next,
             ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-            ['<c-s>'] = open_in_nvim_tree,
           },
         },
       },
